9e5d36b7c33e6243f9bd2f2a2b7b91371c46aaab
Paresh <pareshp1997@gmail.com>
2016-04-05 17:43:02 +0530

    upload added

--- common/models/uploader.js
+++ common/models/uploader.js
@@ -1,16 +1,18 @@
 module.exports = function(Uploader) {
-    Uploader.upload=async function(id,msg){
-    	await Uploader.models.posts.create( {
-  			"name": "string",
-  			"poster": "string",
-			  "description": "string",
-			  "realeaseDate": "2016-03-24",
-			  "startDate": "2016-03-24",
-			  "endDate": "2016-03-24",
+    Uploader.upload=async function(id,name,poster,desc,rd,sd,ed,type,tags){
+        var check=Uploader.find({where:{id:id}})
+      	await Uploader.app.models.posts.create( {
+  			"name": name,
+  			"poster": poster,
+			  "description": desc,
+			  "realeaseDate": rd,
+			  "startDate": sd,
+			  "endDate": ed,
 			  "likes": 0,
-			  "type": true,
-			  "id": "string",
-			  "uploaderId": "id"
+        "tags":tags,
+        "like":[],
+			  "type": type,
+			  "uploaderId": id
         });
     };
     Uploader.recent_feed=async function(id){
@@ -41,6 +43,19 @@ module.exports = function(Uploader) {
     res=await Uploader.app.models.posts.find({where:{uploaderId:id},order:'startDate ASC',limit:1});
     return res;
    }
+   Uploader.remoteMethod('upload',{
+                             accepts:[{arg:'id',type:'string',required:true},
+                                      {arg:'name',type:'string',required:true},
+                                      {arg:'poster',type:'string',required:true},
+                                      {arg:'desc',type:'string',required:true},
+                                      {arg:'rd',type:'Date',required:true},
+                                      {arg:'sd',type:'Date',required:true},
+                                      {arg:'ed',type:'Date',required:true},
+                                      {arg:'type',type:'boolean',required:true},
+                                      {arg:'tags',type:'array',required:true}],
+                             returns:{arg:'res',type:'array'},
+                                 http: {path: '/upload', verb: 'get'}
+                             })
    Uploader.remoteMethod('recent_feed',{
                              accepts:{arg:'id',type:'string',required:true},
                              returns:{arg:'res',type:'array'},
